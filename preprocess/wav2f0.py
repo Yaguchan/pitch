7b61eda49cce21d7cfcaac6773541c88212fb740
Yaguchan <guchi4867@gmail.com>
2023-10-16 17:03:16 +0900

    add preprocess

--- preprocess/wav2f0.py
+++ preprocess/wav2f0.py
@@ -1,35 +1,43 @@
 import os
 import sys
 import numpy as np
+import soundfile as sf
 from tqdm import tqdm
 from world4py.np import apis
 
+# CSJ
+# DATA="/mnt/aoni01/db/CSJ/USB/WAV/core"
+# ATR-Trek
+# DATA="/mnt/aoni04/yaguchi/code/ResponseTimingEstimator_DA/data/ATR_Annotated/make_annotated_data/wav_mono"
+# JSUT
+DATA="/mnt/aoni04/yaguchi/code/pitch_konno/data/jsut_ver1.1/basic5000/wav"
 
-CSJDATA="/mnt/aoni01/db/CSJ/USB/WAV/core"
-OUTDIR="/mnt/aoni04/yaguchi/code/kayanuma/Pitch_aoni04/data/revised/width1_shift1/f0"
-image_shift=1
+# OUTPUT
+OUTDIR="/mnt/aoni04/yaguchi/code/pitch/data/JSUT_basic5000/width1_shift1/f0"
+# wav2spec.py image_shift
+IMAGE_SHIFT=1
 
 
 def getf0(filename):
     x, fs = sf.read(filename, dtype='float64')
     f0, time_axis = apis.dio(x, fs, frame_period=5.0)
     start_frame = 5
-    f0 = f0[start_frame::int(2*image_shift)]
+    f0 = f0[start_frame::int(2*IMAGE_SHIFT)]
     f0 = np.float32(f0)
     return f0
 
 
 def main():
-    names = os.listdir(CSJDATA)
-    names = [name for name in names if name[0]=='A' or name[0]=='S']#頭文字AかSのもののみ
+    names = os.listdir(DATA)
+    # names = [name for name in names if name[0]=='A' or name[0]=='S']#頭文字AかSのもののみ
     os.makedirs(OUTDIR, exist_ok=True)
     for name in tqdm(names):
-        wav_path = os.path.join(CSJDATA, name)
+        wav_path = os.path.join(DATA, name)
         out_path = os.path.join(OUTDIR, name.replace('.wav', '_f0.npy'))
         frames = sf.info(wav_path).frames
         if frames < int(0.1 * 16000):
             continue
-        f0 = getf0(wav_path, out_path=out_path, f0freq=F0FREQ)
+        f0 = getf0(wav_path)
         np.save(out_path, f0)
 
 
--- preprocess/wav2spec.py
+++ preprocess/wav2spec.py
@@ -3,20 +3,28 @@ import wave
 import numpy as np
 import spec_image_torch
 from tqdm import tqdm
-generator = spec_image_torch.SpectrogramImageGeneratorTorch(ramesize=800, frameshift=160, image_width=10, image_shift=5)
+generator = spec_image_torch.SpectrogramImageGeneratorTorch(image_width=1,image_shift=1)
 
 
-CSJDATA = '/mnt/aoni01/db/CSJ/USB/WAV/core'
-OUTPUTDIR = '/mnt/aoni04/yaguchi/code/pitch/data/spectrogram_10'
+# CSJ
+# DATA = '/mnt/aoni01/db/CSJ/USB/WAV/core'
+# ATR-Trek
+# DATA="/mnt/aoni04/yaguchi/code/ResponseTimingEstimator_DA/data/ATR_Annotated/make_annotated_data/wav_mono"
+# JSUT
+DATA="/mnt/aoni04/yaguchi/code/pitch_konno/data/jsut_ver1.1/basic5000/wav"
+
+# OUTPUT
+OUTPUTDIR = '/mnt/aoni04/yaguchi/code/pitch/data/JSUT_basic5000/width1_shift1/spec'
 
 
 def main():
-    names = os.listdir(CSJDATA)
-    names = [name for name in names if name[0]=='A' or name[0]=='S']
+    
+    names = os.listdir(DATA)
+    # names = [name for name in names if name[0]=='A' or name[0]=='S']
     os.makedirs(OUTPUTDIR, exist_ok=True)
     
     for name in tqdm(names):
-        wav_path = os.path.join(CSJDATA, name)
+        wav_path = os.path.join(DATA, name)
         spec_path =  os.path.join(OUTPUTDIR, name.replace('.wav', '_spec.npy'))
         wf = wave.open(wav_path)
         x = np.frombuffer(wf.readframes(wf.getnframes()), np.int16)
